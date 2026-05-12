e149cec0ca0512a8491d7931dc7595e2ea4245ad
huanganfree <huangan1206@163.com>
2026-05-12 17:18:10 +0800

    feat: page search done

--- src/service/system.service.ts
+++ src/service/system.service.ts
@@ -16,24 +16,22 @@ export async function serviceGetUserInfo(ctx: Context) {
 export async function serviceAddRole(ctx: Context) {
     const { userId } = ctx.state.user
     const { roleName, roleCode, description,status } = ctx.request.body as RoleBody
-    const roleIn = await Role.create({ roleName, roleCode,status, description, createdBy: userId, updatedBy: userId });
+    const roleIn = await Role.create({ roleName, roleCode, status, description, createdBy: userId, updatedBy: userId });
     console.log('roleIn.toJSON()=', roleIn.toJSON());
     return roleIn.toJSON() as RoleBody
 }
 
 // 获取角色列表
+/**
+ * offset:页码  limit：数量
+ * @param ctx 
+ * @returns 
+ */
 export async function serviceGetRoles(ctx: Context) {
     const { page = 1, pageSize = 10, roleName = '' } = ctx.request.query
-    console.log('ctx.request.query===', ctx.request.query);
-    
-    const {count, rows} = await Role.findAndCountAll({ offset: +page, limit: +pageSize, where:roleName ? {roleName} : {} });
+    const {count, rows} = await Role.findAndCountAll({ offset: (+page - 1) * (+pageSize), limit: +pageSize, where: roleName ? {  roleName } : {} });
     return {
         total: count,
         records: rows
     }
-
-    // const data = await Role.findAll();
-    // console.log('data=', data.toJSON());
-    // return data
-    
 }
